558198e42ad1a6432c9c8b9564debcfb532a7417
Terry Song <jtsong8@gmail.com>
2016-01-13 07:43:59 -0500

    updated LevelManager.cs using SceneManager

--- backup files/LevelManager_backup.cs
+++ backup files/LevelManager_backup.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 // This script a the game's level manager
 // It manages the flow of different game scenes
@@ -9,14 +8,12 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log("Level load requested for: "+name);
-        // reset breakable brick count
-        Brick.breakable_Count = 0;
+		// reset breakable brick count
+		Brick.breakable_Count = 0;
         // name could be any scene in the game
-        //Application.LoadScene(name); obsolete function
-
-        SceneManager.LoadScene(name);
+        Application.LoadScene(name);
 	}
-
+	
 	public void QuitRequest() {
 		Debug.Log ("Quit requested to leave game");
 		// no effect for debug and web build
@@ -26,9 +23,7 @@ public class LevelManager : MonoBehaviour {
 	public void LoadNextLevel() {
 		// reset breakable brick count
 		Brick.breakable_Count = 0;
-        //Application.LoadLevel (Application.loadedLevel + 1); obsolete
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		Application.LoadLevel (Application.loadedLevel + 1);
 	}
 	
 	public void BrickDestroyed() {
